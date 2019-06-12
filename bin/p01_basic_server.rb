7a9bc481a606de6d089243b724e6f0a7141c4f51
mrkchoi <kennethichoi@gmail.com>
2019-06-12 15:02:56 -0700

    Initial commit

---
+++ bin/p01_basic_server.rb
@@ -0,0 +1,12 @@
+require 'rack'
+
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  res.write('hello world')
+  res.finish
+end
+
+  
